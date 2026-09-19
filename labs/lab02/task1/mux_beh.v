3ee075a3a3354bb997e8938e6a66dcfd01dbddb2
Aditya Balani <f20240361@pilani.bits-pilani.ac.in>
2026-09-19 17:50:09 +0000

    lab02 task1

--- labs/lab02/task1/mux_beh.v
+++ labs/lab02/task1/mux_beh.v
@@ -1,7 +1,7 @@
 // mux_beh.v
 // 2-to-1 multiplexer, BEHAVIORAL style.
 //
-// This file does not compile as-is. Find the bug and fix it before moving on.
+// This file does not compile as-it-is. Find the bug and fix it before moving on.
 // Hint: think carefully about which port should be a net and which should be
 // a variable in behavioral modeling.
 
@@ -9,7 +9,7 @@ module mux_beh (
   input       I0,
   input       I1,
   input       S,
-  output wire Y
+  output reg Y
 );
 
   always @(*) begin
--- labs/lab02/task1/mux_df.v
+++ labs/lab02/task1/mux_df.v
@@ -1,7 +1,7 @@
 // mux_df.v
 // 2-to-1 multiplexer, DATAFLOW style.
 //
-// This file does not compile as-is. Find the bug and fix it before moving on.
+// This file does not compile as-it-is. Find the bug and fix it before moving on.
 // Hint: think carefully about which port should be a net and which should be
 // a variable in dataflow modeling.
 
@@ -9,7 +9,7 @@ module mux_df (
   input      I0,
   input      I1,
   input      S,
-  output reg Y
+  output wire Y
 );
 
   assign Y = S ? I1 : I0;
